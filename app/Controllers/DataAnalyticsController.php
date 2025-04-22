e0e2dd316c79fae37c220ce2785042de5cfcbe14
dady utenga <dadyutenga@yahoo.com>
2025-04-23 00:34:37 +0300

    done

---
+++ app/Controllers/DataAnalyticsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+use App\Models\StudentModel;
+use App\Models\EnrollmentModel;
+use App\Models\PaymentModel;
+use App\Models\AttendanceModel;
+use App\Models\ExamResultModel;
+use App\Models\StaffModel;
+use App\Models\UserModel;
+
+class DataAnalyticsController extends Controller
+{
+    protected $studentModel;
+    protected $enrollmentModel; 
+
+}
+
